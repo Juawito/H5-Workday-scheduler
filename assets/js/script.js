13e0d6652e83cf2aaffd30fcbc295bf81347a165
Juan Santiago <Santiagojuan311@gmail.com>
2021-03-06 21:48:55 -0500

    finshed markdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let currentTimeEl = $('#currentDay');
 let saveBtn = $('.saveBtn');
 let textarea = $('textarea');
 let currentHour = moment().hours();
+//function to display the time in the header 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     currentTimeEl.text(rightNow);
@@ -11,6 +12,7 @@ function displayTime() {
     }
   }
 setInterval(displayTime, 1000);
+// for loop that will compare the current moment in hours to the id assigned to the textareas id and assign classes to them to change the colors
 textarea.each(function (){
     let calendarHour = parseInt($(this).attr('id'));
     if (calendarHour === currentHour){
@@ -23,14 +25,14 @@ textarea.each(function (){
         $(this).addClass('future');
     }
 })
-
+//function that will store the input to local storage
 saveBtn.on('click', function (){
     let userTime = $(this).parent().siblings('textarea').attr('id');
     let userText = $(this).parent().siblings('textarea').val();
     localStorage.setItem(userTime,userText);
-    console.log(userTime);
-    console.log(userText);
 })
+//Gets all of the saved content from local storage and appeneds it to the right id using a for loop
 for (let i = 9; i < 20; i++){
     $('#' + i).val(localStorage.getItem(i));
 }
+
